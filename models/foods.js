db43ba9f8f7aca88ad7ce33d3776b460016c135d
Mike Harvey <mharvey25@gmail.com>
2019-01-09 12:10:44 -0700

    uploading models files

---
+++ models/foods.js
@@ -0,0 +1,51 @@
+module.exports = function (sequelize, DataTypes) {
+    var Food = sequelize.define("Food", {
+        author_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        food_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        recipe: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        },
+        region_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        video_url: {
+            type: DataTypes.TEXT,
+        },
+        country: {
+            type: DataTypes.STRING,
+        },
+        upvotes: {
+            type: DataTypes.SMALLINT,
+            allowNull: false,
+            defaultValue: 1
+        }
+    });
+
+    Food.associate = function (models) {
+       
+        Food.belongsTo(models.Region, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Food;
+};
