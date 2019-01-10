ce43eb102194b5eaa6ecefd6d4adba5fa3cd9ba0
Tola Kasali <kasali56@gmail.com>
2019-01-10 15:27:59 -0700

    commit

--- models/foods.js
+++ models/foods.js
@@ -1,62 +1,58 @@
-$("#submit").on("click", function () {
-    event.preventDefault();
-  
-    if ($("#author-name").val().length < 1 || $("#food-name").val().length < 1 || $("#ingredients").val().length < 1 || $("#directions").val().length < 1 || $("#region").val().length < 1) {
-      alert("You must enter all of areas except for ountry and video");
-      return;
-    };
-  
-     function getRegionId() {
-      var region_name = $("#region").val()
-      if (region_name === "northamerica") {
-        return 1
-      } else if (region_name === "southamerica") {
-        return 2
-      } else if (region_name === "europe") {
-        return 3
-      } else if (region_name === "asia") {
-        return 4
-      } else if (region_name === "africa") {
-        return 5
-      } else if (region_name === "australia") {
-        return 6
-      }
-    }
-  
-    var obj = {
-      author_name: $("#author-name").val(),
-      food_name: $("#food-name").val(),
-      ingredients: $("#ingredients").val(),
-      directions: $("#directions").val(),
-      region_name: $("#region").val(),
-      video_url: $("#video").val(),
-      country: $("#country").val(),
-      RegionId: getRegionId(),
-    };
-  
-  
-    // $.post("/api/foods", obj).then(function (data) {});
-    // $.ajax({
-    //   type: "POST",
-    //   url: "api/foods",
-    //   data: obj
-    // // })
-  
-    console.log(obj);
-    $.ajax("/api/" + obj.region_name, {
-      type: "POST",
-      data: obj
-    }).then(function (response) {
-      console.log(response, `New Region: ${obj}`);
-      // console.log(`New Region: ${obj}`);
-      // if (err) throw err;
-      location.reload();
+module.exports = function (sequelize, DataTypes) {
+    var Food = sequelize.define("Food", {
+        author_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        food_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        ingredients: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        },
+        directions: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
+        },
+        region_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        video_url: {
+            type: DataTypes.TEXT,
+        },
+        country: {
+            type: DataTypes.STRING,
+        },
+        upvotes: {
+            type: DataTypes.SMALLINT,
+            defaultValue: 1
+        }
     });
-  });
-  
-  
-  
-  Collapse 
-  Message Input
-  
-  Message Chris Castro
+
+    Food.associate = function (models) {
+       
+        Food.belongsTo(models.Region, {
+            foreignKey: {
+                allowNull: false
+            }
+        });
+    };
+    return Food;
+};
+
+
+
--- public/js/food.js
+++ public/js/food.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-// Get references to page elements
-var $foodName = $("#food-name");
-var $ingredients = $("#ingredients");
-var $directions = $("#directions");
-var $region = $("#region");
-var $video= $("#video");
-var $submitBtn = $("#submit");
-
-// Ingredients 
-var API = {
-  saveRecipe: function (example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/recipes",
-      data: JSON.stringify(example)
-    });
-  }
-};
-
-=======
 $("#submit").on("click", function () {
   event.preventDefault();
 
