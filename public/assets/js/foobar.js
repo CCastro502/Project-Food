73aae3b2de2ca876a5a9df0df56a521653a4c658
CCastro <chris502castro@gmail.com>
2019-01-14 10:20:25 -0700

    Upvote feature now working

--- public/assets/js/foobar.js
+++ public/assets/js/foobar.js
@@ -1,14 +1,12 @@
-alert("hello");
-
-// $("#upvote").on("click", function() {
-//     if (sessionStorage.length > 0) {
-//         $.ajax("/api/foods/id/" + sessionStorage.getItem("id") + "/upvotes", {
-//             type: "PUT",
-//           }).then(function (response) {
-//             console.log("Upvotes have been updated");
-//             location.reload();
-//           });
-//     } else {
-//         alert("You must be logged in to upvote a recipe");
-//     }
-// })
+$("#upvote").on("click", function() {
+    if (sessionStorage.length > 0) {
+        $.ajax("/api/foods/id/" + sessionStorage.getItem("id") + "/upvotes", {
+            type: "PUT",
+          }).then(function (response) {
+            console.log("Upvotes have been updated");
+            location.reload();
+          });
+    } else {
+        alert("You must be logged in to upvote a recipe");
+    }
+})
