302e7ce2f4868f688c2a65fbe23a6c00930b4932
CCastro <chris502castro@gmail.com>
2019-01-14 18:42:23 -0700

    Made the sign-in button and profile button appear/disappear based on whether the client is logged in or not. Switched 'recipe' for 'ingredients'; also switched 'description' for 'directions'

--- public/assets/js/wholesome.js
+++ public/assets/js/wholesome.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    if (sessionStorage.getItem("id")) {
+    if (sessionStorage.length > 0) {
         $("#profile").html("<li class='create-page nav-pills'><a class='nav-link' href='/profile/" + sessionStorage.getItem("id") + "'>Profile</a></li>")
+    } else {
+        $("#profile").html("<li class='create-page nav-pills'><a class='nav-link' href='/signin'>Log In</a></li>")
     }
 })
